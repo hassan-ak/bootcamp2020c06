d41f86d5f52229c673ebe749ba89f9daf66ebded
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-30 20:23:39 +0500

    Create Components and use it in APP.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { Header } from "./components/header/Header";
+import { Balance } from "./components/balance/Balance";
+import { Summary } from "./components/summary/Summary";
+import { History } from "./components/history/History";
+import { AddTransaction } from "./components/addTransaction/AddTransaction";
+import { Footer } from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
-      Hello world!
+      <Header></Header>
+      <Balance></Balance>
+      <Summary></Summary>
+      <History></History>
+      <AddTransaction></AddTransaction>
+      <Footer></Footer>
     </div>
   );
 }
