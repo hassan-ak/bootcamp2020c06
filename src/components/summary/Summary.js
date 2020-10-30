985a292251fcfe56e86ec1fc64c8ae4af2fb18f9
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-30 21:11:59 +0500

    Update UI for Summary

--- src/components/summary/Summary.js
+++ src/components/summary/Summary.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Summary.css'
 
 export const Summary = () => {
     return (
-        <div>
-            Summary
+        <div className="income-expense">
+            <div>
+                <h4>Income</h4>
+                <p class="income-m">
+                    $ 0.00
+                </p>
+            </div>
+            <div>
+                <h4>Expense</h4>
+                <p class="expense-m">
+                    $ 0.00
+                </p>
+            </div>
         </div>
     )
 }
