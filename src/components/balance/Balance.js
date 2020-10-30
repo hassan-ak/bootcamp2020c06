d336d66e13d40456a8290fd20f05fbac7de0fec6
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-30 20:58:07 +0500

    Updated UI for Balance

--- src/components/balance/Balance.js
+++ src/components/balance/Balance.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Balance.css'
 
 export const Balance = () => {
     return (
-        <div>
-            Balance
+        <div className="balance">
+            <h4>Balance</h4>
+            <h1>
+                <span>-</span>
+                <span>$100</span>
+            </h1>
         </div>
     )
 }
