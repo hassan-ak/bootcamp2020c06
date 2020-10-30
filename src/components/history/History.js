78a7cb03811ec576ecc5f4d4b0725b03045cacbe
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-30 21:21:37 +0500

    Updated UI for Transaction History

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './History.css'
 
 export const History = () => {
     return (
-        <div>
-            History
+        <div className="transaction-history">
+            <h3>Transaction History</h3>
+            <ul class="list">
+                <li class="income-h">In<span>$100</span>
+                    <button class="del">X</button>
+                </li>
+                <li class="expense-h">Out<span>-$100</span>
+                    <button class="del">X</button>
+                </li>
+                <li class="income-h">In1<span>$250</span>
+                    <button class="del">X</button>
+                </li>
+                <li class="expense-h">Out1<span>-$70</span>
+                    <button class="del">X</button>
+                </li>
+            </ul>
         </div>
     )
 }
