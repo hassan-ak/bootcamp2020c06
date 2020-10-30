b7f6222eabef8223c9f781668db36df3f134f928
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-30 23:21:33 +0500

    Update Balance.js

--- src/components/balance/Balance.js
+++ src/components/balance/Balance.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../../hooks/GlobalContext';
 import './Balance.css'
 
 export const Balance = () => {
+    const context = useContext(GlobalContext)
+    const amounts = context.transactions.map(transaction => transaction.amount) ;
+    const total = amounts.reduce((acc,item)=>(acc+=item),0);
+    const sign = total<0? '-':'';
     return (
         <div className="balance">
             <h4>Balance</h4>
             <h1>
-                <span>-</span>
-                <span>$100</span>
+                <span>{sign}</span>
+                <span>${(Math.abs(total)).toFixed(2)}</span>
             </h1>
         </div>
     )
