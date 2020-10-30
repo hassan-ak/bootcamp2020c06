10a057945a32683931380e98088beff22b634c46
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-30 22:08:03 +0500

    Create react hooks
    Define Initial States and Global Context

---
+++ src/hooks/GlobalContext.js
@@ -0,0 +1,7 @@
+// React Imports
+import { createContext } from 'react'
+//Hooks Imports
+import initialState from './InitialState'
+
+// Create Context
+export const GlobalContext = createContext(initialState);
