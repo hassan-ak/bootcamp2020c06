d9882f638c586bf65d9ea5e838208d1344446e2a
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-30 21:46:38 +0500

    Updated UI for AddTransaction

--- src/components/addTransaction/AddTransaction.js
+++ src/components/addTransaction/AddTransaction.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import './AddTransaction.css'
 
 export const AddTransaction = () => {
     return (
         <div>
-            Add transaction
+            <h3>
+                Add Transactions
+            </h3>
+            <form>
+                <div>
+                    <label htmlFor="description">
+                        Description
+                    </label>
+                    <input  type="text" 
+                            placeholder="Enter Description"/>
+                    <small>
+                        Kindly Enter Description
+                    </small>
+                </div>
+                <div>
+                    <label htmlFor="amount">
+                        Amount
+                    </label>
+                    <input  type="number"
+                            step="0.00000000001" 
+                            placeholder="Enter Amount"/>
+                    <small>
+                        Kindly Enter Amount
+                    </small>
+                </div>
+                <div className="btn">
+                    <button className="add-income"> 
+                        Add Income
+                    </button>
+                    <button className="add-expense">
+                        Add Expense
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
